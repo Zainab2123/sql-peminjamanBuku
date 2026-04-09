aba8273dcf80fba9b75c4526d465f196d2cd500d
Zainab2123 <zainabgfhisoka@gmail.com>
2026-04-08 21:13:55 -0700

    sql terbaru lagi

--- 5/peminjaman_buku.sql
+++ 5/peminjaman_buku.sql
@@ -1,14 +1,16 @@
--- phpMyAdmin SQL Dump -- version 5.2.1
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2026 at 11:42 AM
+-- Generation Time: Apr 09, 2026 at 06:13 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -22,8 +24,9 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `anggota`
+-- Table structure for table `anggota`
 --
+
 CREATE TABLE `anggota` (
   `id_anggota` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
@@ -34,17 +37,19 @@ CREATE TABLE `anggota` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `anggota`
+-- Dumping data for table `anggota`
 --
+
 INSERT INTO `anggota` (`id_anggota`, `id_user`, `nama`, `kelas`, `no_hp`, `status`) VALUES
 (1, 7, 'zainab', 'xii rpl', '12345678910', 'aktif'),
-(2, 8, 'user66663', '', '', 'nonaktif');
+(2, 8, 'user66663', '', '', 'aktif');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `buku`
+-- Table structure for table `buku`
 --
+
 CREATE TABLE `buku` (
   `id_buku` int(11) NOT NULL,
   `judul` varchar(255) NOT NULL,
@@ -57,23 +62,29 @@ CREATE TABLE `buku` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `buku`
+-- Dumping data for table `buku`
 --
+
 INSERT INTO `buku` (`id_buku`, `judul`, `penulis`, `penerbit`, `tahun_terbit`, `stok`, `gambar_buku`, `id_kategori`) VALUES
-(4, 'MATEMATIKA Untuk SMA/MA Kelas X', 'Dr. Andi Pratama dan Dra. Siti Nuraini', 'Dinamika Edukasi', '2026', 44, 'MATEMATIKA Untuk Kelas X.png', 1),
+(4, 'MATEMATIKA Untuk SMA/MA Kelas X', 'Dr. Andi Pratama dan Dra. Siti Nuraini', 'Dinamika Edukasi', '2026', 40, 'MATEMATIKA Untuk Kelas X.png', 1),
 (5, 'MATEMATIKA Untuk SMA/MA Kelas XI', 'Dr. Andi Pratama dan Dra. Siti Nuraini', 'Dinamika Edukasi', '2026', 45, 'MATEMATIKA Untuk SMAMA Kelas XI.png', 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_peminjaman`
+-- Table structure for table `detail_peminjaman`
 --
+
 CREATE TABLE `detail_peminjaman` (
   `id_detail` int(11) NOT NULL,
   `id_peminjaman` int(11) NOT NULL,
   `id_buku` int(11) NOT NULL,
   `jumlah` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detail_peminjaman`
+--
 
 INSERT INTO `detail_peminjaman` (`id_detail`, `id_peminjaman`, `id_buku`, `jumlah`) VALUES
 (8, 8, 4, 1),
@@ -83,17 +94,24 @@ INSERT INTO `detail_peminjaman` (`id_detail`, `id_peminjaman`, `id_buku`, `jumla
 (12, 12, 4, 2),
 (13, 13, 4, 2),
 (14, 14, 4, 2),
-(15, 15, 4, 2);
+(15, 15, 4, 2),
+(16, 16, 4, 3),
+(17, 17, 4, 4);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori`
+-- Table structure for table `kategori`
 --
+
 CREATE TABLE `kategori` (
   `id_kategori` int(11) NOT NULL,
   `nama_kategori` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori`
+--
 
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 (1, 'Matematika'),
@@ -112,8 +130,9 @@ INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `peminjaman`
+-- Table structure for table `peminjaman`
 --
+
 CREATE TABLE `peminjaman` (
   `id_peminjaman` int(11) NOT NULL,
   `id_anggota` int(11) NOT NULL,
@@ -122,14 +141,11 @@ CREATE TABLE `peminjaman` (
   `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `peminjaman`
+--
+
 INSERT INTO `peminjaman` (`id_peminjaman`, `id_anggota`, `tanggal_peminjaman`, `tanggal_kembali`, `status`) VALUES
-(1, 1, '2026-04-07', '2026-04-07', 'dikembalikan'),
-(2, 1, '2026-04-07', '2026-04-07', 'dikembalikan'),
-(3, 1, '2026-04-07', '2026-04-08', 'dikembalikan'),
-(4, 2, '2026-04-07', '2026-04-07', 'dikembalikan'),
-(5, 1, '2026-04-08', '2026-04-08', 'dikembalikan'),
-(6, 1, '2026-04-08', '', 'dipinjam'),
-(7, 1, '2026-04-08', '', 'dipinjam'),
 (8, 1, '2026-04-01', '2026-04-08', 'dikembalikan'),
 (9, 1, '2026-04-01', '2026-04-08', 'dikembalikan'),
 (10, 1, '2026-04-08', '', 'dipinjam'),
@@ -137,19 +153,26 @@ INSERT INTO `peminjaman` (`id_peminjaman`, `id_anggota`, `tanggal_peminjaman`, `
 (12, 1, '2026-04-08', '', 'dipinjam'),
 (13, 1, '2026-04-08', '', 'dipinjam'),
 (14, 1, '2026-04-08', '', 'dipinjam'),
-(15, 1, '2026-04-08', '', 'dipinjam');
+(15, 1, '2026-04-08', '', 'dipinjam'),
+(16, 2, '2026-04-08', '2026-04-08', 'dikembalikan'),
+(17, 2, '2026-04-08', '', 'dipinjam');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
+
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `role`) VALUES
 (1, 'siswa', 'siswa123', 'anggota'),
@@ -161,57 +184,119 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `role`) VALUES
 (7, 'zainab', '$2y$10$.vc1tjPRz/UYK4/nwYiPv.WkyAPlBoBFVfFgiYWz9TLPRJTMh7fBi', 'anggota'),
 (8, 'putri', '$2y$10$ypNsKLqHshBc6LJEknw6DOELbidB60vHaEFFBJbqMJK0sjvDp8ZbK', 'anggota');
 
--- --------------------------------------------------------
-
 --
--- INDEXES & CONSTRAINTS
+-- Indexes for dumped tables
 --
 
+--
+-- Indexes for table `anggota`
+--
 ALTER TABLE `anggota`
   ADD PRIMARY KEY (`id_anggota`),
   ADD KEY `id_user` (`id_user`);
 
+--
+-- Indexes for table `buku`
+--
 ALTER TABLE `buku`
   ADD PRIMARY KEY (`id_buku`),
   ADD KEY `id_kategori` (`id_kategori`);
 
+--
+-- Indexes for table `detail_peminjaman`
+--
 ALTER TABLE `detail_peminjaman`
   ADD PRIMARY KEY (`id_detail`),
   ADD KEY `id_peminjaman` (`id_peminjaman`),
   ADD KEY `id_buku` (`id_buku`);
 
+--
+-- Indexes for table `kategori`
+--
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
+--
+-- Indexes for table `peminjaman`
+--
 ALTER TABLE `peminjaman`
   ADD PRIMARY KEY (`id_peminjaman`),
   ADD KEY `id_anggota` (`id_anggota`);
 
+--
+-- Indexes for table `user`
+--
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
--- AUTO_INCREMENT
-ALTER TABLE `anggota` MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-ALTER TABLE `buku` MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-ALTER TABLE `detail_peminjaman` MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-ALTER TABLE `kategori` MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-ALTER TABLE `peminjaman` MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-ALTER TABLE `user` MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
--- FOREIGN KEY
+--
+-- AUTO_INCREMENT for table `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `detail_peminjaman`
+--
+ALTER TABLE `detail_peminjaman`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `anggota`
+--
 ALTER TABLE `anggota`
   ADD CONSTRAINT `anggota_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `buku`
+--
 ALTER TABLE `buku`
   ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `detail_peminjaman`
+--
 ALTER TABLE `detail_peminjaman`
   ADD CONSTRAINT `detail_peminjaman_ibfk_1` FOREIGN KEY (`id_peminjaman`) REFERENCES `peminjaman` (`id_peminjaman`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `detail_peminjaman_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `peminjaman`
+--
 ALTER TABLE `peminjaman`
   ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`) ON DELETE CASCADE ON UPDATE CASCADE;
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
